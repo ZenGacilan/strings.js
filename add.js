c6065fe42c2e4eb71dc4501f04e8f8d6304c8c09
ZenGacilan <potapetekau@gmail.com>
2024-09-13 09:31:32 +0800

    initial commit

---
+++ add.js
@@ -0,0 +1,18 @@
+const favActorFirstName = "Tom";
+const favActorLastName = "Holland";
+
+const fullName = favActorFirstName + "Tom" + favActorLastName "Holland";
+
+const uppercase = fullName.toUpperCase("TOM HOLLAND");
+const lowercase = ("tom holland");
+
+let message = "My favorite actor is "Tom Holland" + uppercase + "TOM HOLLAND";
+
+message += " His best show is Spiderman Far From Home.";
+
+console.log("Tom:", favActorFirstName);
+console.log("Holland:", favActorLastName);
+console.log("Tom Holland:", fullName);
+console.log("TOM HOLLAND:", uppercase);
+console.log("tessa:", lowercase);
+console.log("Tom Holland is a Actor in the movie Spiderman Far From Home:", message);
